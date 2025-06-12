d25e88aa519f6cb730df909fe5198be027a4ddb1
2382034 <2382034@unai.edu>
2025-06-12 13:18:16 +0700

    Update auth.controller.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,21 @@
+// src/auth/auth.controller.ts
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDTO } from './dto/register.dto';
+import { Public } from './public.decorator'; // 1. Impor decorator @Public
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
-  @Post('login')
-async signIn(@Body() loginDto: LoginDto) { // Ganti signInDto menjadi loginDto
-  // Ganti ini:
-  // return this.authService.signIn(loginDto.email, loginDto.password);
-  // Menjadi ini (menggunakan metode login yang sudah ada):
-  return this.authService.login(loginDto);
-}
 
+  @Public() // 2. Tandai endpoint ini sebagai publik
+  @Post('login')
+  async signIn(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
+  }
+
+  @Public() // 3. Tandai endpoint ini juga sebagai publik
   @Post('register')
   register(@Body() registerDto: RegisterDTO) {
     return this.authService.register(registerDto);
